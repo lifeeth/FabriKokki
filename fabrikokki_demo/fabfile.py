699f0d697bbfea5898a2ef5b375acbbdb28d8efb
Steve Steiner <ssteinerX@gmail.com>
2010-08-10 13:16:22 -0400

    Working do-nothing fabfile

--- fabrikokki_demo/fabfile.py
+++ fabrikokki_demo/fabfile.py
@@ -1,5 +1,22 @@
-from fabric.api import *
+# August 10, 2010 -- ss
+#
+#   Just getting it all to run, not pulling in any of our provider
+#   replacements.
 
-def init_kokki():
+from fabric.api import *
+import kokki
+import fabrikokki as fk
+
+def _init_kokki(recipe_dir='./cookbooks'):
+    """
+    Initializes our Kokki instance
+
+    TODO:   pull this out to the FabriKokki class and initialize that once we
+            figure out what it has to do.
+    """
+    pass
 
 def install_recipe(recipe, params):
+    pass
+
+_init_kokki()
